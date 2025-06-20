e4082a443ecb7b8f8eac271a55b29ec0097fb34d
ashim <ashimgautam01@gmail.com>
2025-06-20 17:29:33 +0545

    refactor: clean up appointment and user authentication controllers for improved readability and consistency

--- Hospital_M_S/backend/controllers/appoint.controller.js
+++ Hospital_M_S/backend/controllers/appoint.controller.js
@@ -5,153 +5,136 @@ import Doctor from "../models/doctor.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/AsyncHandler.js";
-import User from '../models/user.js'
+import User from "../models/user.js";
 
 const bookAppointment = asyncHandler(async (req, res) => {
-    const { d_id, name, age, date, doctorName, problem, phone } = req.body;
-    if (!d_id || !name || !age || !date || !doctorName || !problem) {
-        throw new ApiError(401, "All fields are required");
-    }
+  const { d_id, name, age, date, doctorName, problem, phone } = req.body;
+  if (!d_id || !name || !age || !date || !doctorName || !problem) {
+    throw new ApiError(401, "All fields are required");
+  }
 
-    const doctor = await Doctor.findById(d_id);
-    if (!doctor) {
-        throw new ApiError(401, "No doctor found");
-    }
+  const doctor = await Doctor.findById(d_id);
+  if (!doctor) {
+    throw new ApiError(401, "No doctor found");
+  }
 
-    if (doctor.date.includes(date)) {
-        throw new ApiError(400, "Invalid date as Date is already booked");
-    }
+  if (doctor.date.includes(date)) {
+    throw new ApiError(400, "Invalid date as Date is already booked");
+  }
 
-    const appoint = await Appoint.create({
-        doctorid: d_id,
-        name,
-        age,
-        doctorName,
-        problem,
-        date,
-        phone,
-        email: req.user.email,
-        user: req.user._id
-    });
+  const appoint = await Appoint.create({
+    doctorid: d_id,
+    name,
+    age,
+    doctorName,
+    problem,
+    date,
+    phone,
+    email: req.user.email,
+    user: req.user._id,
+  });
 
-    doctor.date.push(date);
-    await doctor.save();
+  doctor.date.push(date);
+  await doctor.save();
 
-    const medicalHistoryEntry = {
-        problem: problem,
-        response: "",
-        medicine: "",
-        doctorname: doctorName,
-        Date: date
-    };
+  const medicalHistoryEntry = {
+    problem: problem,
+    response: "",
+    medicine: "",
+    doctorname: doctorName,
+    Date: date,
+  };
 
-    const user = await User.findById(req.user._id);
-    if (user) {
-        user.medicalHistory.push(medicalHistoryEntry);
-        await user.save();
-    }
-    const app=await Appoint.findById(appoint._id)
-    const appointid=app.appointmentId
-   
-    const html = appointmentHtml(doctorName, problem, date,appointid);
-    await sendemail(req.user.email, 'Appointment Confirmation', html);
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.medicalHistory.push(medicalHistoryEntry);
+    await user.save();
+  }
+  const app = await Appoint.findById(appoint._id);
+  const appointid = app.appointmentId;
 
-    const updatedDoctor = await Doctor.findById(d_id);
+  const html = appointmentHtml(doctorName, problem, date, appointid);
+  await sendemail(req.user.email, "Appointment Confirmation", html);
 
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "Appointment Booked Successfully",
-            {
-                appoint: appoint,
-                doctor: updatedDoctor
-            }
-        )
-    );
+  const updatedDoctor = await Doctor.findById(d_id);
+
+  return res.status(200).json(
+    new ApiResponse(200, "Appointment Booked Successfully", {
+      appoint: appoint,
+      doctor: updatedDoctor,
+    })
+  );
 });
 
+const getAppointmentofUser = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const patient = await Appoint.findById(id);
 
+  if (!patient) {
+    throw new ApiError(400, "No patient found");
+  }
 
-const getAppointmentofUser=asyncHandler(async(req,res)=>{
-    const id=req.params.id
-     const patient=await Appoint.findById(id)
+  const profile = await User.findById(patient.user).select(
+    "-password -ismember -isverified -otp -otp_expiry -refreshToken "
+  );
 
-    if(!patient){
-        throw new ApiError(400,"No patient found")
-    }
-    
-    const profile=await User.findById(patient.user).select("-password -ismember -isverified -otp -otp_expiry -refreshToken ")
- 
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "Appointment found",
-            {
-            "patient":patient,
-            "user":profile
-        }
-        )
-    )
-})
+  return res.status(200).json(
+    new ApiResponse(200, "Appointment found", {
+      patient: patient,
+      user: profile,
+    })
+  );
+});
 
 const updateAppointment = asyncHandler(async (req, res) => {
+  const { id, medicine } = req.body;
 
-    const { id, medicine } = req.body;
- 
-    const appointment = await Appoint.findById(id);
-    if (!appointment) {
-        return res.status(404).json(new ApiResponse(404, "Appointment not found"));
-    }
+  const appointment = await Appoint.findById(id);
+  if (!appointment) {
+    return res.status(404).json(new ApiResponse(404, "Appointment not found"));
+  }
 
-    appointment.medicine=medicine
+  appointment.medicine = medicine;
 
-    await appointment.save({validateBeforeSav:false})
+  await appointment.save({ validateBeforeSav: false });
 
-    const updatedAppointment = await Appoint.findById(id)
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "User updated",
-            {
-                appoint: updatedAppointment
-            }
-        )
-    );
+  const updatedAppointment = await Appoint.findById(id);
+  return res.status(200).json(
+    new ApiResponse(200, "User updated", {
+      appoint: updatedAppointment,
+    })
+  );
 });
-
 
 const checkedAppointments = asyncHandler(async (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const user = await Appoint.findById(id);
-    user.doctorid=null
-    user.save();
-    if (!user) {
-        return res.status(404).json(new ApiResponse(404, "Appointment not found"));
-    }
+  const id = req.params.id;
+  console.log(id);
+  const user = await Appoint.findById(id);
+  user.doctorid = null;
+  user.save();
+  if (!user) {
+    return res.status(404).json(new ApiResponse(404, "Appointment not found"));
+  }
 
-    return res.status(200).json(
-        new ApiResponse(200, "Checked Out", [])
-    );
+  return res.status(200).json(new ApiResponse(200, "Checked Out", []));
 });
 
-const pharmacy=asyncHandler(async(req,res)=>{
-    const appointmentId=req.params.id
-    const getAppointments=await Appoint.findOne({appointmentId})
-    if(!getAppointments){
-        throw new ApiError(
-            400,
-            "no appointments found",
-        )
-    }
-  
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "Appointment found",
-            getAppointments
-        )
-    )
-})
+const pharmacy = asyncHandler(async (req, res) => {
+  const appointmentId = req.params.id;
+  const getAppointments = await Appoint.findOne({ appointmentId });
+  if (!getAppointments) {
+    throw new ApiError(400, "no appointments found");
+  }
 
-export {bookAppointment,getAppointmentofUser,checkedAppointments,pharmacy,updateAppointment}
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Appointment found", getAppointments));
+});
+
+export {
+  bookAppointment,
+  getAppointmentofUser,
+  checkedAppointments,
+  pharmacy,
+  updateAppointment,
+};
--- Hospital_M_S/backend/controllers/user.authController.js
+++ Hospital_M_S/backend/controllers/user.authController.js
@@ -1,286 +1,279 @@
-import asyncHandler from '../utils/AsyncHandler.js'
-import ApiError from '../utils/ApiError.js'
-import ApiResponse from '../utils/ApiResponse.js'
-import User from '../models/user.js'
-import uploadOnCloud from '../utils/cloudinary.js'
-import otpHtml from '../Emails/html/otpVerify.js'
-import sendemail from '../Emails/SendEmail.js'
+import asyncHandler from "../utils/AsyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import User from "../models/user.js";
+import uploadOnCloud from "../utils/cloudinary.js";
+import otpHtml from "../Emails/html/otpVerify.js";
+import sendemail from "../Emails/SendEmail.js";
 
+const generateAccessAndRefreshToken = async (userId) => {
+  const user = await User.findById(userId);
+  const refreshToken = user.generateRefreshToken();
+  const accessToken = user.generateAccessToken();
+  user.refreshToken = refreshToken;
+  await user.save({ validateBeforeSave: false });
+  return { refreshToken, accessToken };
+};
 
-const generateAccessAndRefreshToken=async(userId)=>{
-    const user=await User.findById(userId)
-      const refreshToken=user.generateRefreshToken();
-    const accessToken=user.generateAccessToken()
-    user.refreshToken=refreshToken;
-    await user.save({validateBeforeSave:false})
-    return {refreshToken,accessToken}
-}
+const generateOtp = () => {
+  const otp = Math.floor(Math.random() * 999999);
+  const otp_expiry = new Date(Date.now() + 20 * 60 * 1000);
+  return { otp, otp_expiry };
+};
 
-const generateOtp=()=>{
-    const otp=Math.floor(Math.random()*999999);
-    const otp_expiry=new Date(Date.now()+20*60*1000)
-    return {otp,otp_expiry}
-}
-
-const sendingEmail=async(email,otp,otp_expiry)=>{
-    try {
-        const otp_html=otpHtml(otp,otp_expiry);
-        const sendEmailVerification=await sendemail(email,"otp verification",otp_html)
-        if(!sendEmailVerification){
-            throw new ApiError(400,"Error to send email")
-        }
-    } catch (error) {
-       console.log(error); 
+const sendingEmail = async (email, otp, otp_expiry) => {
+  try {
+    const otp_html = otpHtml(otp, otp_expiry);
+    const sendEmailVerification = await sendemail(
+      email,
+      "otp verification",
+      otp_html
+    );
+    if (!sendEmailVerification) {
+      throw new ApiError(400, "Error to send email");
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 const RegisterUser = asyncHandler(async (req, res) => {
-    const { name, age, email, password } = req.body;
+  const { name, age, email, password } = req.body;
 
-    if ([name, age, email, password].some(field => !field.trim())) {
-        throw new ApiError(400, "All fields are required");
+  if ([name, age, email, password].some((field) => !field.trim())) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    if (existingUser.isverified) {
+      throw new ApiError(401, "Email already exists");
+    } else {
+      return res.status(201).json(new ApiResponse(201, "Verify user", []));
     }
+  }
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        if (existingUser.isverified) {
-            throw new ApiError(401, "Email already exists");
-        } else {
-            return res.status(201).json(new ApiResponse(201, "Verify user", []));
-        }
-    }
+  const localProfilePath = req.files?.profile?.[0]?.path;
+  if (!localProfilePath) {
+    throw new ApiError(400, "Failed to select photo");
+  }
 
-    const localProfilePath = req.files?.profile?.[0]?.path;
-    if (!localProfilePath) {
-        throw new ApiError(400, "Failed to select photo");
-    }
+  const response = await uploadOnCloud(localProfilePath);
+  if (!response) {
+    throw new ApiError(500, "Failed to upload photo");
+  }
 
-    const response = await uploadOnCloud(localProfilePath);
-    if (!response) {
-        throw new ApiError(500, "Failed to upload photo");
-    }
-    
-    const profile = response.url;
-    const { otp, otp_expiry } = generateOtp();
+  const profile = response.url;
+  const { otp, otp_expiry } = generateOtp();
 
-    const newUser = await User.create({
-        name,
-        age,
-        email,
-        otp,
-        otp_expiry,
-        password,
-        profile,
-    });
+  const newUser = await User.create({
+    name,
+    age,
+    email,
+    otp,
+    otp_expiry,
+    password,
+    profile,
+  });
 
-    await sendingEmail(email, otp, otp_expiry);
+  await sendingEmail(email, otp, otp_expiry);
 
-    const savedUser = await User.findById(newUser._id).select("-password -ismember -isverified -otp -otp_expiry -refreshToken -medicalHistory");
-    if (!savedUser) {
-        throw new ApiError(500, "Failed to save user in database");
-    }
+  const savedUser = await User.findById(newUser._id).select(
+    "-password -ismember -isverified -otp -otp_expiry -refreshToken -medicalHistory"
+  );
+  if (!savedUser) {
+    throw new ApiError(500, "Failed to save user in database");
+  }
 
-    return res.status(200).json(new ApiResponse(200, "User registered successfully", savedUser));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "User registered successfully", savedUser));
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-        throw new ApiError(400, "All fields are required");
-    }
+  if (!email || !password) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    const user = await User.findOne({ email });
-    if (!user) {
-        throw new ApiError(400, "No email is registered. Please register first.");
-    }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(400, "No email is registered. Please register first.");
+  }
 
-    if (!user.isverified) {
-        throw new ApiError(400, "Email is not verified yet.");
-    }
+  if (!user.isverified) {
+    throw new ApiError(400, "Email is not verified yet.");
+  }
 
-    const authorizedUser = await user.ispasswordCorrect(password);
-    if (!authorizedUser) {
-        throw new ApiError(401, "Invalid credentials.");
-    }
+  const authorizedUser = await user.ispasswordCorrect(password);
+  if (!authorizedUser) {
+    throw new ApiError(401, "Invalid credentials.");
+  }
 
-    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
-    const options = {
-        http: true,
-        secure: true
-    };
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+  const options = {
+    http: true,
+    secure: true,
+  };
 
-    const userWithToken = await User.findById(authorizedUser._id)
-        .select("-password -ismember -isverified -otp -otp_expiry -refreshToken -medicalHistory");
+  const userWithToken = await User.findById(authorizedUser._id).select(
+    "-password -ismember -isverified -otp -otp_expiry -refreshToken -medicalHistory"
+  );
 
-    return res
-        .status(200)
-        .cookie("refreshToken", refreshToken, options)
-        .cookie("accessToken", accessToken, options)
-        .json(new ApiResponse(200, "User logged in", {
-            user: userWithToken,
-            accessToken,
-            refreshToken
-        }));
-});
-
-const verifyOpt=asyncHandler(async(req,res)=>{
-    const id=req.params.id
-    
-    const {otp}=req.body
-    const user=await User.findById(id)
-
-    if(user.isverified){
-        throw new ApiError(400,"user with email is already verified")
-    }
-    const presentDate=new Date(Date.now())
-    if(presentDate>user.otp_expiry){
-        throw new ApiError(400,"Opt has expired please get new otp")
-    }
-    console.log(user.otp);
-    if(user.otp!=otp){
-       throw new ApiError(400,"Invalid otp")
-    }
-    user.isverified=true
-    user.otp=null
-    user.otp_expiry=null
-    await user.save({validateBeforeSave:false})
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "user verification successful",
-            user.name
-        )
-    )
-})
-
-const resendEmail=asyncHandler(async(req,res)=>{
-    const id=req.params.id
-    const user=await User.findById(id)
-    if(user.isverified){
-        throw new ApiError(400,"user is already verified")
-    }
-    const {otp,otp_expiry}=generateOtp()
-    await sendingEmail(user.email,otp,otp_expiry)
-    const updatedUser=await User.findOneAndUpdate({_id:id},
-        {
-            $set:{otp:otp,otp_expiry:otp_expiry}
-        },
-        {
-            new:true
-        }
-    )
-    return res.status(200).json(
-       new ApiResponse(
-        200,
-        "Email sent successfully",
-        updatedUser.name,
-       )
-    )
-})
-
-const changePassword=asyncHandler(async(req,res)=>{
-    const id=req.params.id
-    const {password}=req.body
-    const user=await User.findById(id)
-    if(user.password===password){
-        throw new ApiError(401,"old password and new are same")
-    }
-    user.password=password
-    await user.save()
-    if(!user){
-        throw new ApiError(400,"password cannot be updated")
-    }
-    return res.status(200)
-    .json(
-        new ApiResponse(
-            200,
-            "password has been changed",
-            []
-        )
-    )
-})
-
-const logoutUser=asyncHandler(async(req,res)=>{
-    await User.findOneAndUpdate(
-        {$set:{refreshToken:undefined}}
-    )
-    const options={
-        http:true,
-        secure:true
-    }
-
-    return res
+  return res
     .status(200)
-    .clearCookie("refreshToken",options)
-    .clearCookie("accessToken",options)
+    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
     .json(
-        new ApiResponse(
-            200,
-            "User logged Out",
-            []
-        )
-    )
-})
-
-const getUser=asyncHandler(async(req,res)=>{
-    const id=req.user._id 
-    const user=await User.findById(id).select("-password -isverified -otp -otp_expiry -refreshToken -medicalHistory  -createdAt -updatedAt -__v")
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "user is verified",
-            {
-                user:user
-            }
-        )
-    )
-})
-
-const updateUser = asyncHandler(async (req, res) => {
-
-    const { id,problem, response, medicine } = req.body;
- 
-    const user = await User.findById(id);
-    if (!user) {
-        return res.status(404).json(new ApiResponse(404, "User not found"));
-    }
-
-    const medicalEntry = {
-        problem,
-        response,
-        medicine
-    };
-
-    user.medicalHistory.push(medicalEntry);
-    await user.save({ validateBeforeSave: false });
-
-    const updatedUser = await User.findById(id).select("-password -ismember -isverified -otp -otp_expiry -refreshToken");
-
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "User updated",
-            {
-                user: updatedUser
-            }
-        )
+      new ApiResponse(200, "User logged in", {
+        user: userWithToken,
+        accessToken,
+        refreshToken,
+      })
     );
 });
 
+const verifyOpt = asyncHandler(async (req, res) => {
+  const id = req.params.id;
 
-const getHistory=asyncHandler(async(req,res)=>{
-    const id=req.params.id
-    const user=await User.findById(id).select("-password -ismember -isverified -otp -otp_expiry -refreshToken ")
-    return res.status(200).json(
-        new ApiResponse(
-            200,
-            "user is verified",
-            {
-                user:user.medicalHistory
-            }
-        )
-    )
-})
+  const { otp } = req.body;
+  const user = await User.findById(id);
 
+  if (user.isverified) {
+    throw new ApiError(400, "user with email is already verified");
+  }
+  const presentDate = new Date(Date.now());
+  if (presentDate > user.otp_expiry) {
+    throw new ApiError(400, "Opt has expired please get new otp");
+  }
+  console.log(user.otp);
+  if (user.otp != otp) {
+    throw new ApiError(400, "Invalid otp");
+  }
+  user.isverified = true;
+  user.otp = null;
+  user.otp_expiry = null;
+  await user.save({ validateBeforeSave: false });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "user verification successful", user.name));
+});
 
-export {RegisterUser,loginUser,logoutUser,getUser,verifyOpt,resendEmail,changePassword,updateUser,getHistory}
+const resendEmail = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id);
+  if (user.isverified) {
+    throw new ApiError(400, "user is already verified");
+  }
+  const { otp, otp_expiry } = generateOtp();
+  await sendingEmail(user.email, otp, otp_expiry);
+  const updatedUser = await User.findOneAndUpdate(
+    { _id: id },
+    {
+      $set: { otp: otp, otp_expiry: otp_expiry },
+    },
+    {
+      new: true,
+    }
+  );
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Email sent successfully", updatedUser.name));
+});
+
+const changePassword = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const { password } = req.body;
+  const user = await User.findById(id);
+  if (user.password === password) {
+    throw new ApiError(401, "old password and new are same");
+  }
+  user.password = password;
+  await user.save();
+  if (!user) {
+    throw new ApiError(400, "password cannot be updated");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "password has been changed", []));
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findOneAndUpdate({ $set: { refreshToken: undefined } });
+  const options = {
+    http: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("refreshToken", options)
+    .clearCookie("accessToken", options)
+    .json(new ApiResponse(200, "User logged Out", []));
+});
+
+const getUser = asyncHandler(async (req, res) => {
+  const id = req.user._id;
+  const user = await User.findById(id).select(
+    "-password -isverified -otp -otp_expiry -refreshToken -medicalHistory  -createdAt -updatedAt -__v"
+  );
+  return res.status(200).json(
+    new ApiResponse(200, "user is verified", {
+      user: user,
+    })
+  );
+});
+
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, problem, response, medicine } = req.body;
+
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(404).json(new ApiResponse(404, "User not found"));
+  }
+
+  const medicalEntry = {
+    problem,
+    response,
+    medicine,
+  };
+
+  user.medicalHistory.push(medicalEntry);
+  await user.save({ validateBeforeSave: false });
+
+  const updatedUser = await User.findById(id).select(
+    "-password -ismember -isverified -otp -otp_expiry -refreshToken"
+  );
+
+  return res.status(200).json(
+    new ApiResponse(200, "User updated", {
+      user: updatedUser,
+    })
+  );
+});
+
+const getHistory = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const user = await User.findById(id).select(
+    "-password -ismember -isverified -otp -otp_expiry -refreshToken "
+  );
+  return res.status(200).json(
+    new ApiResponse(200, "user is verified", {
+      user: user.medicalHistory,
+    })
+  );
+});
+
+export {
+  RegisterUser,
+  loginUser,
+  logoutUser,
+  getUser,
+  verifyOpt,
+  resendEmail,
+  changePassword,
+  updateUser,
+  getHistory,
+};
